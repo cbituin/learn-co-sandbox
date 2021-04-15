7faaa894e85e1c7de084ccc97d8cd8349d322a3a
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2021-04-15 10:52:16 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,13 @@
+contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_ice_cream_flavors: ["chocolate", "vanilla"]  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
+
+puts contacts["Jon Snow"].:favorite_ice_cream_flavors << "strawberry"
